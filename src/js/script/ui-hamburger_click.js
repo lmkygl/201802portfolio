682ab651f3ec9d68bd5045e70d44f0b40ad4b0d7
geunyoung <limgeunyoung90@gmail.com>
2018-03-27 15:08:19 +0900

    navi js 작업

--- src/js/script/ui-hamburger_click.js
+++ src/js/script/ui-hamburger_click.js
@@ -1,4 +1,11 @@
 // hamburger button 클릭시 네비게이션 보임 
 (function(){
+    var navi__btn = ('[data-ui="hamburger__btn"]');
+    var navi__area = ('[data-ui="navi__area"]');
+
+    function navigation__click(){
+        console.log(111);
+    }
     
+    navi__btn.on('click', navigation__click)
 }());
