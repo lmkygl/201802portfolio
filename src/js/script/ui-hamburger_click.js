96470cd81c701f4cc057cc9bd76d42afcf19e6cd
geunyoung <limgeunyoung90@gmail.com>
2018-03-27 16:21:32 +0900

    hamburger

--- src/js/script/ui-hamburger_click.js
+++ src/js/script/ui-hamburger_click.js
@@ -2,17 +2,31 @@
 (function () {
     var $navi__btn = $('[data-ui="hamburger__btn"]');
     var $navi__list__area = $('[data-ui="navi__area"]');
+    var $span__before = $('.span__before');
+    var $span__after = $('span__after');
+    TweenMax.to($navi__list__area, 0.2, {
+        left: '-100%'
+    });
 
     function navigation__click(e) {
         console.log(11);
-        TweenMax.to($navi__list__area, 0.2,{left:'5%'});
-       $(this).toggleClass('m_hamburgercloseactive');
-
+        $(this).toggleClass('m_hamburgercloseactive');
         if ($(this).hasClass('m_hamburgercloseactive')) {
-           TweenMax.to($navi__list__area, 0.2,{left:'5%'});
+            TweenMax.to($navi__list__area, 0.2, {
+                left: '5%'
+            });
+            TweenMax.to($span__before , 0.2, {
+                rotation: -45,
+                top:40
+            });
+            TweenMax.to($span__after , 0.2, {
+                rotation: 45,
+                bottom:40
+            });
         } else {
-            $navi__list__area.hide();
-           TweenMax.to($navi__list__area, 0.2,{left:'-5%'});
+            TweenMax.to($navi__list__area, 0.2, {
+                left: '100%'
+            });
         }
     }
 
