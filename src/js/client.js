8c86da0241fc2e0066607796796351947cb17082
geunyoung <limgeunyoung90@gmail.com>
2018-03-27 15:59:51 +0900

    hamburger btn 작업중

--- src/js/client.js
+++ src/js/client.js
@@ -1,2 +1,2 @@
-console.log("module2");
+console.log("module2"),function(){var a=$('[data-ui="hamburger__btn"]'),e=$('[data-ui="navi__area"]');a.on("click",function(a){e.show(),$(this).toggleClass("m_hamburgercloseactive"),$(this).hasClass("m_hamburgercloseactive")?e.show():e.hide()})}();
 //# sourceMappingURL=client.js.map
--- src/js/script/ui-hamburger_click.js
+++ src/js/script/ui-hamburger_click.js
@@ -1,11 +1,12 @@
 // hamburger button 클릭시 네비게이션 보임 
-(function(){
-    var navi__btn = ('[data-ui="hamburger__btn"]');
-    var navi__area = ('[data-ui="navi__area"]');
+(function () {
+    var $navi__btn = $('[data-ui="hamburger__btn"]');
+    var $navi__list__area = $('[data-ui="navi__area"]');
 
-    function navigation__click(){
-        console.log(111);
+    function navigation__click(e) {
+        
+     
     }
-    
-    navi__btn.on('click', navigation__click)
+
+    $navi__btn.on('click', navigation__click);
 }());
