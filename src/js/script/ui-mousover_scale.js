47ef90c9ac380a5aa01dbdb1bdb38164d107934d
geunyoung <limgeunyoung90@gmail.com>
2018-03-28 17:59:49 +0900

    sns scale 작업중

--- src/js/script/ui-mousover_scale.js
+++ src/js/script/ui-mousover_scale.js
@@ -1,4 +1,11 @@
 //mouseover sacle 커짐 transfrom 대신 tweenmax 사용
 (function(){
-    
+    var $scale = $('[data-ui="scale1"]');
+
+    function mouseover__scale(){
+        TweeMax.to($scale,0.3,{
+            scale: 1, 
+        });
+    }
+    $scale.on('mouseenter mouseleave', mouseover__scale);
 }());
