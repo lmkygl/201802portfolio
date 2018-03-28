8318be42e762298f9e5dd075401eb8e7da7272b1
geunyoung <limgeunyoung90@gmail.com>
2018-03-28 19:02:54 +0900

    sns 영역 마우스 오버시 스케일기능 트윈맥스 이용

--- src/js/script/ui-mousover_scale.js
+++ src/js/script/ui-mousover_scale.js
@@ -1,12 +1,20 @@
 //mouseover sacle 커짐 transfrom 대신 tweenmax 사용
 (function(){
-    var $scale = $('[data-ui="scale1"]');
+    var $scale = $('[data-ui="scale"]');
 
     function mouseover__scale(e){
-        console.log(111);
-        TweeMax.to($scale,0.3,{
-            scale: 1.2, 
-        });
+        var $target = $(e.target);
+        var $event__type = e.type;
+        
+        if($event__type === 'mouseenter'){
+            TweenMax.to($(this),0.2,{
+                scale: 1.1, 
+            });
+        }else{
+            TweenMax.to($(this),0.3,{
+                scale: 1, 
+            });
+        }
     }
     $scale.on('mouseenter mouseleave', mouseover__scale);
 }());
