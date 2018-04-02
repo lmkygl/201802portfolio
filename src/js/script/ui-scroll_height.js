8fe0074762901279cfdd9373b01dd07677a0a01b
geunyoung <limgeunyoung90@gmail.com>
2018-04-02 18:46:34 +0900

    scroll height 값

--- src/js/script/ui-scroll_height.js
+++ src/js/script/ui-scroll_height.js
@@ -1,102 +1,110 @@
-// //스크롤 높이값을 이용한 기능 
-// (function () {
-//     var $area = $('[data-ui-animate="section__area"]');
-//     var $bg_position = $('[data-ui-position="background__position"]');
-//     var $bg_opacity = $('[data-ui="bg__opacity"]');
-//     var $content_position = $('[data-ui="position"]');
-//     var $fade = $('[data-ui="fade"]');
-//     var isScrolling;
-//     var lastScroll;
+//스크롤 높이값을 이용한 기능 
+(function () {
+    var $area = $('[data-ui-animate="section__area"]');
+    var $bg_position = $('[data-ui-position="background__position"]');
+    var $bg_opacity = $('[data-ui="bg__opacity"]');
+    var $content_position = $('[data-ui="position"]');
+    var $fade = $('[data-ui="fade"]');
+    var isScrolling;
+    var lastScroll;
 
 
-//     function scroll_event() {
-//         TweenMax.set($bg_position, {
-//             backgroundPosition: '50% 60%',
-//             // ease: Circ.easeOut
-//         });
-//         TweenMax.set($bg_opacity, {
-//             opacity: 0.7
-//         });
-//         TweenMax.set($content_position, {
-//             bottom: 0,
-//             opacity: 1
-//         });
+    function scroll_inital(){
+        TweenMax.set($bg_position, {
+            backgroundPosition: '50% 0',
+            ease: Circ.easeOut
+        });
+        TweenMax.set($bg_opacity, {
+            opacity: 0
+        });
+        TweenMax.set($content_position, {
+           opacity: 1
+        });
 
-//     }
+    }
 
-//     function scroll_fade_event() {
-//         TweenMax.to($fade, 0.2,{
-//             opacity:1
-//         });
-//     }
+    function scroll_event() {
+        TweenMax.to($bg_position, 0.3, {
+            backgroundPosition: '50% 60%',
+            ease: Circ.easeOut
+        });
+        TweenMax.to($bg_opacity, 0.3, {
+            opacity: 0.7
+        });
+        TweenMax.to($content_position, 0.3, {
+            bottom: 0,
+            opacity: 1
+        });
 
-//     // 스크롤이 해당 영역에 닿았을시 발생되는 공통 애니메이션 
-//     function common_Animation(index) {
-//         var $ui_motion = $area.eq(index).find('[data-ui-position]');
-//         TweenMax.to($ui_motion, 1, {
-//             backgroundPosition: '50% 0'
-//         });
-//         TweenMax.set($bg_opacity, {
-//             opacity: 0
-//         });
-//         TweenMax.set($content_position, {
-//             top: 0,
-//             opacity: 1
-//         });
-//         TweenMax.to($fade, 0.2,{
-//             opacity:0
-//         });
-//     }
+    }
 
+    function scroll_fade_event() {
+        TweenMax.to($fade, 0.2,{
+            opacity:1
+        });
+    }
 
-//     function init() {
-//         // scroll_ain();
-//         $area.each(function () {
-//             // $(this).attr('속성 (id, class, src)', 넣고자하는 값);
-//             $(this).attr('data-offset', $(this).offset().top - $(window).height() / 2);
-//         });
-//     }
+    // 스크롤이 해당 영역에 닿았을시 발생되는 공통 애니메이션 
+    function common_Animation(index) {
+        var $ui_motion = $area.eq(index).find('[data-ui-position]');
+        TweenMax.to($ui_motion, 1, {
+            backgroundPosition: '50% 60%',
+            ease: Power1.easeOut
+        });
+        // TweenMax.set($bg_opacity, {
+        //     opacity: 0
+        // });
+        // TweenMax.set($content_position, {
+        //     top: 0,
+        //     opacity: 1
+        // });
+        
+    }
 
-//     $(window).on('load scroll', function () {
-//         var scrollTop = $(window).scrollTop();
-//         var scrollDiff = scrollTop - lastScroll;
+    
+    function init() {
+        // scroll_ain();
+        $area.each(function () {
+            // $(this).attr('속성 (id, class, src)', 넣고자하는 값);
+            $(this).attr('data-offset', $(this).offset().top - $(window).height() / 2);
+        });
+    }
+    $(window).on('load', init);
 
-//         console.log(scrollTop);
-//         if (scrollTop >= $area.eq(0).data('offset') && scrollTop < $area.eq(1).data('offset')) {
-//           //  scroll_event(0);
+    $(window).on('load scroll', function () {
+        var scrollTop = $(window).scrollTop();
+        var scrollDiff = scrollTop - lastScroll;
 
-//             if (scrollDiff > 0) {
+        console.log(scrollTop);
+        if (scrollTop >= $area.eq(0).data('offset') && scrollTop < $area.eq(1).data('offset')) {
+            scroll_event();
 
-//                 scroll_event(0);
-//             } else {
+            if (scrollDiff > 0) {
+                scroll_inital();
+            } else {
+                scroll_event();
+            }
 
-//                 common_Animation(0);
-//             }
-//         } else if (scrollTop >= $area.eq(1).data('offset') && scrollTop < $area.eq(2).data('offset')) {
+        } else if (scrollTop >= $area.eq(1).data('offset') && scrollTop < $area.eq(2).data('offset')) {
 
-//             if (scrollDiff < 0) {
+            if (scrollDiff > 0) {
+                scroll_fade_event();
+            } else {
+                common_Animation();
+            }
+        } else if (scrollTop <= $area.eq(2).data('offset')) {
+            // scroll_event(2);
+            if (scrollDiff < 0) {
+                scroll_event();
+            } else {
+                common_Animation();
+            }
+        }
 
-//                 scroll_fade_event(0);
-//             } else {
+        // clearTimeout(isScrolling);
+        // isScrolling = setTimeout(function(){
+        //     lastScroll = $(window).scrollTop();
+        // },300);
 
-//                 common_Animation(0);
-//             }
-//         } else if (scrollTop > $area.eq(2).data('offset')) {
-//             // scroll_event(2);
-//             if (scrollDiff < 0) {
-
-//                 scroll_event(2);
-//             } else {
-
-//                 common_Animation(0);
-//             }
-//         }
-
-//         // clearTimeout(isScrolling);
-//         // isScrolling = setTimeout(function(){
-//         //     lastScroll = $(window).scrollTop();
-//         // },300);
-
-//     });
-//     $(window).on('load', init);
-// }());
+    });
+}());
