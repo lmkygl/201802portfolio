9f7d8e2747a1e75bb189eac61d1bf24d73902da1
geunyoung <limgeunyoung90@gmail.com>
2018-04-04 20:32:02 +0900

    Update 2018-04-04T11:31:59.982Z

--- js/script/ui-toggle_box.js
+++ js/script/ui-toggle_box.js
@@ -1,27 +1,30 @@
 // 버튼 클릭시 하단의 상품 보여줌
 (function () {
     var $button = $('[data-ui-btn="toggle_box"]').find('.toggle__btn');
-    var toggled = false;
+    // var toggled = false;
     var animating = false;
     var speed = 0.2;
 
     function toggle_effect(e) {
         var $target = $(e.target);
+        var toggled =  $target.data('toggle');
         var $sponsorship_box = $(e.target).parents('[data-ui-btn="toggle_box"]');
         var $product_area = $sponsorship_box.find('[data-ui="sponsorship_product_area"]');
+
+        console.log($target.data);
 
         if (!animating) {
             animating = true;
 
             if (!toggled) {
                 $product_area.slideDown(function(){
-                    toggled = true;
+                    $target.data('toggle', true).attr('data-toggle', true);                   
                     animating = false;
                 });
                 $target.text("▲");
             } else {
                 $product_area.slideUp(function(){
-                    toggled = false;
+                    $target.data('toggle', false).attr('data-toggle', false);                   
                     animating = false;
                 });
                 $target.text("▼");
