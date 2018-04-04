80ab3de8f90bf4e6f0a92744ca3069a55da04403
geunyoung <limgeunyoung90@gmail.com>
2018-04-04 18:01:31 +0900

    ontv.html js css 마크업 완성

--- src/js/script/ui-toggle_box.js
+++ src/js/script/ui-toggle_box.js
@@ -1,47 +1,47 @@
 // 버튼 클릭시 하단의 상품 보여줌
-(function(){
+(function () {
     var $button = $('[data-ui-btn="toggle_box"]').find('.toggle__btn');
     var toggled = false;
     var animating = false;
-    
-    function toggle_effect(e){
+
+    function toggle_effect(e) {
         var $target = $(e.target);
         var $sponsorship_box = $(e.target).parents('[data-ui-btn="toggle_box"]');
         var $product_area = $sponsorship_box.find('[data-ui="sponsorship_product_area"]');
-       console.log($product_area);
         if (!animating) {
             animating = true;
             if (!toggled) {
                 toggled = true;
                 $sponsorship_box.animate({
-                    "height":"530px",
-                    'border-Bottom':'none'
+                    "height": "530px",
+                    'border-Bottom': 'none'
                 });
                 $product_area.animate({
-                    'opacity':'1',
+                    'opacity': '1',
                 });
                 $target.text("▲");
-                return ;
+                return;
             }
         }
-        if (animating){
+        if (animating) {
             animating = false;
 
             if (toggled) {
                 toggled = false;
                 $sponsorship_box.animate({
-                    "height":"300px"
+                    "height": "300px"
                 });
                 $product_area.animate({
-                    "opacity":"0"
+                    "opacity": "0",
+                    'top': '0'
                 });
                 $target.text("▼");
                 return;
             }
         }
 
-        
+
     }
-    
+
     $button.on('click', toggle_effect);
 }());
