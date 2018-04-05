9137d04a241e28335cfa7ef64bfa506dce3ec923
geunyoung <limgeunyoung90@gmail.com>
2018-04-05 21:59:52 +0900

    list_filter

--- src/js/script/ui-list_filter.js
+++ src/js/script/ui-list_filter.js
@@ -1,17 +1,17 @@
 // search 매장명, 주소 입력시 그 해당 객체만 보여줌
 
 (function () {
-    var $search_text = $('[data-ui="search_text"]');
-    var $search_btn = $('[data-ui="search_btn"]');
+    var $search_text = $('[data-ui="search__text"]');
+    var $search_btn = $('[data-ui="search__btn"]');
 
     function list_search(e) {
         var $search_val = $search_text.val();
-        var $list = $('.store__list').find('li');
+        var $list = $('.offline__list').find('li');
         if ($search_val == '') {
             $list.show();
         } else {
             $list.hide();
-            $('.store__list li:contains("' + $search_val + '")').show();
+            $('.offline__list li:contains("' + $search_val + '")').show();
         }
     }
     $search_btn.on('click', list_search);
