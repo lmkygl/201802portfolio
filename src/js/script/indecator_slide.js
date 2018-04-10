8a5dc05bc7ac79d19453da5ef892c3d28814d4f1
geunyoung <limgeunyoung90@gmail.com>
2018-04-10 16:56:16 +0900

    indecator.js 기능 추가

--- src/js/script/indecator_slide.js
+++ src/js/script/indecator_slide.js
@@ -7,14 +7,12 @@
     var animating = false;
     var index = 0;
     
-    function slide_show(e, index) {
+    function indecator_slide_show(e, index) {
         // indecator index 
         if (e !== null) {
             // event 값이 널값이 아닐 경우
             var $target = $(e.target);
             var $target_index = $target.index();
-            // clearInterval(interval);
-            // console.log($target_index);
             $indicator_btn.removeClass('select_indicator');
             $indicator_btn.eq($target_index).addClass('select_indicator');
 
@@ -47,11 +45,11 @@
                 index = 0;
             }
             indecator_animating(index);
-            slide_show(null, index);
+            indecator_slide_show(null, index);
         }, 2000);
     }
 
-    $indicator_btn.on('click', slide_show);
+    $indicator_btn.on('click', indecator_slide_show);
 
     $(window).on('load blur focus', function(e) {
         if (e.type === 'blur') {
@@ -61,5 +59,5 @@
         }
     });
 
-    
+
 }());
