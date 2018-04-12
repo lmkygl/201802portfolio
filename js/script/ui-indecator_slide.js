5a7ace4ff4125eb6873f90c126514ed19609b1ca
geunyoung <limgeunyoung90@gmail.com>
2018-04-12 18:41:31 +0900

    Update 2018-04-12T09:41:23.661Z

---
+++ js/script/ui-indecator_slide.js
@@ -0,0 +1,63 @@
+// indecator slide 
+(function(){
+    var interval;
+    var $indicator = $('.indicator');
+    var $indicator_btn = $indicator.find('span');
+    var speed = 300;
+    var animating = false;
+    var index = 0;
+    
+    function indecator_slide_show(e, index) {
+        // indecator index 
+        if (e !== null) {
+            // event 값이 널값이 아닐 경우
+            var $target = $(e.target);
+            var $target_index = $target.index();
+            $indicator_btn.removeClass('select_indicator');
+            $indicator_btn.eq($target_index).addClass('select_indicator');
+
+            indecator_animating($target_index);
+            
+        } else {
+            $indicator_btn.removeClass('select_indicator');
+            $indicator_btn.eq(index).addClass('select_indicator');
+           
+        }
+    }
+
+    function indecator_animating(idx) {
+        // slide animate 
+        var $side_out_frame = $('[data-ui="side_slide_out_frame"]');
+        var $side_out_frame_width = $side_out_frame.width();
+        var $side_inner_frame = $('[data-ui="side_slide_inner_frame"]');
+
+        $side_inner_frame.animate({
+            marginLeft: -$side_out_frame_width * idx
+        }, speed, 'swing');
+    }
+    
+    function startInterval() {
+        clearInterval(interval);
+        interval = setInterval(function() {
+            // var index = 0; 
+            index++;
+            if (index == 3) {
+                index = 0;
+            }
+            indecator_animating(index);
+            indecator_slide_show(null, index);
+        }, 2000);
+    }
+
+    $indicator_btn.on('click', indecator_slide_show);
+
+    $(window).on('load blur focus', function(e) {
+        if (e.type === 'blur') {
+            clearInterval(interval);
+        } else {
+            startInterval();
+        }
+    });
+
+
+}());
