adb6f842552032bb97ee5bdb8c1fd4c0103c4a09
geunyoung <limgeunyoung90@gmail.com>
2018-04-12 18:28:22 +0900

    prodct_sub.html

--- src/js/script/ui-tab_fade.js
+++ src/js/script/ui-tab_fade.js
@@ -1,5 +1,5 @@
 //tab 버튼 클릭시 fade in out
-(function(){
+(function () {
     var $data_tab_id = $('[data-id]');
     var $tab_wrap = $('[data-ui="tab_button_wrap"]');
     var $tab_btn = $tab_wrap.find('.tab__btn');
@@ -16,28 +16,21 @@
         $tab_btn.removeClass('select__tab');
         $tab_btn.eq(tab_id).addClass('select__tab');
 
-        console.log($tab_con);
         if (tab_id == 0) {
-            // $tab_con.eq(1).show();
             $tab_con.fadeOut();
             $tab_con.eq(0).fadeIn();
             $tab_img.fadeOut();
             $tab_img.eq(0).fadeIn();
-            // return false;
         } else if (tab_id == 1) {
-            // $tab_con.eq(1).show();
             $tab_con.fadeOut();
             $tab_con.eq(1).fadeIn();
             $tab_img.fadeOut();
             $tab_img.eq(1).fadeIn();
-            // return false;
         } else if (tab_id == 2) {
-            // $tab_con.eq(1).show();
             $tab_con.fadeOut();
             $tab_con.eq(2).fadeIn();
             $tab_img.fadeOut();
             $tab_img.eq(2).fadeIn();
-            // return false;
         }
     }
     $tab_btn.on('click', tab_button);
