f6905755e6c23ecc71182039245e78e6ae98c32d
geunyoung <limgeunyoung90@gmail.com>
2018-04-12 18:19:54 +0900

    product_sub 기능 연결

--- src/js/script/ui-tab_fade.js
+++ src/js/script/ui-tab_fade.js
@@ -2,7 +2,7 @@
 (function(){
     var $data_tab_id = $('[data-id]');
     var $tab_wrap = $('[data-ui="tab_button_wrap"]');
-    var $tab_btn = $tab_wrap.find('.btn');
+    var $tab_btn = $tab_wrap.find('.tab__btn');
     var speed = 200;
 
     function tab_button(e) {
@@ -10,28 +10,34 @@
         var tab_id;
         // var tab_content;
         var $tab_con = $('.btn_content').find('li');
-        console.log($target);
+        var $tab_img = $('.img_content').find('li');
 
         tab_id = $target.attr('data-id');
         $tab_btn.removeClass('select__tab');
         $tab_btn.eq(tab_id).addClass('select__tab');
 
-        
+        console.log($tab_con);
         if (tab_id == 0) {
             // $tab_con.eq(1).show();
             $tab_con.fadeOut();
             $tab_con.eq(0).fadeIn();
-            return false;
+            $tab_img.fadeOut();
+            $tab_img.eq(0).fadeIn();
+            // return false;
         } else if (tab_id == 1) {
             // $tab_con.eq(1).show();
             $tab_con.fadeOut();
             $tab_con.eq(1).fadeIn();
-            return false;
+            $tab_img.fadeOut();
+            $tab_img.eq(1).fadeIn();
+            // return false;
         } else if (tab_id == 2) {
             // $tab_con.eq(1).show();
             $tab_con.fadeOut();
             $tab_con.eq(2).fadeIn();
-            return false;
+            $tab_img.fadeOut();
+            $tab_img.eq(2).fadeIn();
+            // return false;
         }
     }
     $tab_btn.on('click', tab_button);
